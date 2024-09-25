554a23adca9d82006990ac6a2684ef54fc8093c3
yaroslav22842 <Ayaroslav2011@gmail.com>
2024-09-25 17:33:31 +0300

    Fixed another homework

--- Project4/Project4/.venv/Main.py
+++ Project4/Project4/.venv/Main.py
@@ -1,21 +1,29 @@
 
-try:
-    result = []
-    def divider(a, b):
+
+result = []
+def divider(a, b):
+    try:
         if a < b:
             raise ValueError
         if b > 100:
             raise IndexError
             return a/b
-
-    data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
-except TypeError:
-    print("Somethings went wrong.")
+    except NameError:
+        print("Incorrect name")
+    except TypeError:
+        print("Incorrect type")
 
 try:
+    data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
     for key in data:
         res = divider(key, data[kem])
         result.append(res)
         print(result)
+
+except TypeError:
+    print("Incorrect data")
 except NameError:
-    print("Incorrect name")
+    print("Incorrect Name")
+
+
+
